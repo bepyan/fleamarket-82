c3bd7221f2e22474659b0debae2d925f227eae98
bepyan <rlantia97@kumoh.ac.kr>
2020-11-20 14:15:46 +0900

    Updates

---
+++ precache-manifest.c7b85582193237d061c9bb7f000a2d02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959d78fd9d2baac78578b68863ab8562",
+    "url": "/fleamarket-82/index.html"
+  },
+  {
+    "revision": "64e7403f2d7d89dab9d3",
+    "url": "/fleamarket-82/static/css/main.6e9ebeb3.chunk.css"
+  },
+  {
+    "revision": "700cbf221619d6b64da3",
+    "url": "/fleamarket-82/static/js/2.4f9942ba.chunk.js"
+  },
+  {
+    "revision": "b1ed51dba99a7c28e0382e7e538bb8d0",
+    "url": "/fleamarket-82/static/js/2.4f9942ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e7403f2d7d89dab9d3",
+    "url": "/fleamarket-82/static/js/main.cc455732.chunk.js"
+  },
+  {
+    "revision": "7ecc3893d93396396509",
+    "url": "/fleamarket-82/static/js/runtime-main.337f3e98.js"
+  },
+  {
+    "revision": "90ac46548e307504e998b778231d4c5a",
+    "url": "/fleamarket-82/static/media/logo_1.90ac4654.png"
+  },
+  {
+    "revision": "400044b69868c86da441bb47dbaf5136",
+    "url": "/fleamarket-82/static/media/school2.400044b6.csv"
+  }
+]);
